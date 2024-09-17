67bfa0b923f40e288cec6dd413bc45cf10074785
Georgi Angelov <gangelov@asteasolutions.com>
2024-09-17 13:49:00 +0300

    chore: fix export

--- packages/scratch-gui/src/index.ts
+++ packages/scratch-gui/src/index.ts
@@ -1,4 +1,4 @@
-export {default as GUI} from './containers/gui.jsx';
+export {default} from './containers/gui.jsx';
 export {default as AppStateHOC} from './lib/app-state-hoc.jsx';
 export {remixProject} from './reducers/project-state.js';
 export {setAppElement} from 'react-modal';
