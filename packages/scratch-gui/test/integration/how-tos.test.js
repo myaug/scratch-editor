7f2f8102b3782c1e763dd303ecd816fe91d13468
Kaloyan Manolov <kaloyan.manolov@advance-auto.com>
2024-09-30 14:04:13 +0300

    fix: uepr-8: fix test

--- packages/scratch-gui/test/integration/how-tos.test.js
+++ packages/scratch-gui/test/integration/how-tos.test.js
@@ -27,7 +27,7 @@ describe('Working with the how-to library', () => {
         await loadUri(uri);
         await clickText('Costumes');
         await clickXpath('//*[@aria-label="Tutorials"]');
-        await clickText('Getting Started'); // Modal should close
+        await clickText('Add a Sprite'); // Modal should close
         // Make sure YouTube video on first card appears
         await findByXpath('//div[contains(@class, "step-video")]');
         const logs = await getLogs();
