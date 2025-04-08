05bb0f845b36fb3e33be812cb8cd767298b369cd
Kaloyan Manolov <kmanolov@asteasolutions.com>
2025-04-08 11:33:06 +0300

    feat: remove unused import

--- packages/scratch-gui/src/index.ts
+++ packages/scratch-gui/src/index.ts
@@ -3,9 +3,6 @@ import {legacyConfig} from './legacy-config';
 
 export {default} from './containers/gui.jsx';
 export {default as AppStateHOC} from './lib/app-state-hoc.jsx';
-// TODO: This doesn't appear to be used in scratch-desktop anymore
-// Remove if unneeded.
-export {default as TitledHoc} from './lib/titled-hoc.jsx';
 export {remixProject} from './reducers/project-state.js';
 export {setAppElement} from 'react-modal';
 
